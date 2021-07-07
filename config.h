91421954deb93aadb1eb0aff65aa805011582bec
thaoeu <thaoeu@thaoeu.site>
2021-07-07 16:29:31 +0800

    Enter Mod+v to vim

--- config.h
+++ config.h
@@ -209,7 +209,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_c,           normalMode,     {.i =  0} },
+	{ MODKEY,               XK_v,           normalMode,     {.i =  0} },
 };
 
 /*
